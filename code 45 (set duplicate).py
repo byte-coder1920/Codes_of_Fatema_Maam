dec01a5f004affc63bf2cb9e4ba3b920825f7279
byte-coder1920 <abrarislam765@gmail.com>
2024-12-31 16:39:09 +0600

    Add files via upload

--- code 45 (set duplicate).py
+++ code 45 (set duplicate).py
@@ -1,6 +1,11 @@
 # set duplicate
-set1 = {1, 2, 3, 4}
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
 
-set2 = set1.copy()
+common_elements = set1 & set2
+print(common_elements)
 
-print(set2)
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+common_elements = set1.intersection(set2)
+print(common_elements)
