502f6f0a7272d7f60c1e331dd60daffcb5f2b77d
byte-coder1920 <abrarislam765@gmail.com>
2025-01-03 11:42:11 +0600

    Update Series 3.py

--- CP Assignment 2/Series 3.py
+++ CP Assignment 2/Series 3.py
@@ -1,8 +1,12 @@
 #series
-#2*2+4*4+6*6+.....+n
-
+#2*2+4*2+8*2+.....+n
 n= int(input("enter the Last number: "))
-sum=1
-for x in range(2,n+1,2):
-    sum =sum*(x**2)
+sum=0
+term=2
+
+for x in range(1, n + 1):
+    if term>n:
+        break
+    sum+=term**2
+    term*=2
 print(sum)
