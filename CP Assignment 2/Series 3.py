fcb32ae3bfdae08b90ee74b7c80316b60ebffef8
byte-coder1920 <abrarislam765@gmail.com>
2025-01-06 23:51:08 +0600

    Update Series 3.py

--- CP Assignment 2/Series 3.py
+++ CP Assignment 2/Series 3.py
@@ -1,12 +1,11 @@
 #series
 #2*2+4*2+8*2+.....+n
-n= int(input("enter the Last number: "))
-sum=0
-term=2
+n = int(input("Enter the position (n): "))
+sum = 0
+term = 2
 
-for x in range(1, n + 1):
-    if term>n:
-        break
-    sum+=term**2
-    term*=2
-print(sum)
+for i in range(1, n + 1):
+    sum += term ** 2
+    term *= 2
+
+print("The sum of the series till the nth position is:", sum)
